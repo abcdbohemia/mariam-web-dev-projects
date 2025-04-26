c1199eb91ce6e99a8884272e3232ccf03afad760
Picaso <abcdbohemia@gmail.com>
2025-04-26 23:25:52 +0400

    feat (weather) : Implement API data fetching Fetches weather data from  OpenWeatherMap API using the city input. Handles successful responses and errors, updating relevant state. API key is sourced from env variables.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,15 +7,31 @@ import WeatherDisplay from './components/WeatherDisplay';
 function App() {
   const [city, setCity] = useState('');
   const[weatherData, setWeatherData] = useState(null);
-  const [error, serError] = useState(null); //state to hold any error messages
+  const [error, setError] = useState(null); //state to hold any error messages
 
-  const handleSearch = (newCity) => { setCity(newCity);
-  //we'll add the api call logic later
+  const handleSearch = async (newCity) => { 
+    setCity(newCity);
+    setWeatherData(null);
+    setError(null);
+
   const apiKey = process.env.OPENWEATHER_API_KEY; //access the env variable (we saved api key through git bash earlier)
-  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=metric`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}&units=metric`;
   console.log(`Searching for weather in: ${newCity}`);
-  }
 
+  try {
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    const message = `HTTP error! status: ${response.status}`;
+    throw new Error(message);
+  }
+  const data = await response.json();
+  setWeatherData(data);
+  } catch (error) { console.error( `Fetching weather data failed:`, error);
+  setError('Failed to fetch weather data. Please try again.');
+  setWeatherData(null); //ensure weatherData is null on error
+  }
+  };
+  
   return (
     <div className="app-container">
       <h1>Weather App</h1>
