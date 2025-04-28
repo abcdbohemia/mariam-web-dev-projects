d19315002bc64a009336caabc269a05b65e68a8a
Picaso <abcdbohemia@gmail.com>
2025-04-28 20:33:08 +0400

    feat: Use Geocoding API for city to coordinates conversion
    The OpenWeatheMap API no longer directly supports weather data retrieval by city name. This commit integrates the Geocoding API to first convert the user-entered city name into latitude and longitude, which are then used to fetch the current weather data.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -15,18 +15,34 @@ function App() {
     setError(null);
 
   const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY; //access the env variable (we saved api key through git bash earlier)
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&appid=${apiKey}&units=metric`;
-  console.log(`Searching for weather in: ${newCity}`);
+  const geocodingApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${newCity}&limit=1&appid=${apiKey}`;
 
   try {
-  const response = await fetch(apiUrl);
-  if (!response.ok) {
-    const message = `HTTP error! status: ${response.status}`;
+  const geoResponse = await fetch(geocodingApiUrl);
+  if (!geoResponse.ok) {
+    const message = `Geocoding API error! status: ${geoResponse.status}`;
     throw new Error(message);
   }
-  const data = await response.json();
-  setWeatherData(data);
-  } catch (error) { console.error( `Fetching weather data failed:`, error);
+  const geoData = await geoResponse.json();
+
+  if (geoData & geoData.length > 0) {
+    const { lat, lon } = geoData[0];
+    console.log('Coordinates found:', lat, lon);
+  
+  const weatherApiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}&units=metric';
+  const weatherResponse = await fetch(weatherApiUrl);
+  if (!weatherResponse.ok) {
+    const message = `Weather API error! status: ${weatherResponse.status}`;
+    throw new Error(message);
+  }
+  const weatherData = await weatherResponse.json();
+  setWeatherData(weatherData);
+} else {
+  setError('City not found. Please try again.');
+  setWeatherData(null);
+}
+  } catch (error) { 
+    console.error( `Fetching data failed:`, error);
   setError('Failed to fetch weather data. Please try again.');
   setWeatherData(null); //ensure weatherData is null on error
   }
