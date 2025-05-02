72b63485bc7793eab7e369691998488037b2f6ff
Picaso <abcdbohemia@gmail.com>
2025-05-02 15:41:54 +0400

    feat: moved animated folder to assets

--- weather-app/src/components/WeatherDisplay.js
+++ weather-app/src/components/WeatherDisplay.js
@@ -1,6 +1,7 @@
 import React from "react";
 import weatherDescriptions from './weatherCodes';
 import weatherIconMapping from './weatherIconMapping';
+import './WeatherDisplay.css'
 
 function WeatherDisplay({ weather, error, city }) {
   if (error) {
@@ -10,10 +11,16 @@ function WeatherDisplay({ weather, error, city }) {
   if (weather) {
     const description = weatherDescriptions[weather.weather_code] // Using the value of weather.weather_code as a key to look up a corresponding value within the weatherDescription object.
     || "Description not available";
+    const iconFile = weatherIconMapping[weather.weather_code] || 'default.svg'; // Get the icon file name, with a default fallback 
+    const iconPath = `/assets/animated/${iconFile}`; //construct the path to the icon 
+    
     return (
       <div className="weather-info">
         <h2>{city}</h2> {/* Placeholder for city/location */}
-        <p className="temperature">Temperature: {Math.round(weather.temperature)}°C</p>
+        <div className="weather-details">
+          {iconPath && <img src={iconPath} alt={description} className="weather-icon" />} {/*Display the icon*/}
+          <p className="temperature">Temperature: {Math.round(weather.temperature)}°C</p>
+        </div>
         {weather.weather_code && (
           <p className="description">Weather: {description} (Code: {weather.weather_code})</p>
         )}
