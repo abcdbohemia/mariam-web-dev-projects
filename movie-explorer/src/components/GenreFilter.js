3497e1081113e157b47a37ef0c8de4f3bce89874
Picaso <abcdbohemia@gmail.com>
2025-05-08 22:22:22 +0400

    Feat: Add GenreFilter component

--- movie-explorer/src/components/GenreFilter.js
+++ movie-explorer/src/components/GenreFilter.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import './GenreFilter.css';
 import SearchBar from './components'
 
-function GenreFilter(onFilter){
-movies.map
+function GenreFilter({ genres, onFilter, selectedGenre }){
+return (
+    <div className="genre-filter">
+        <h2>Filter by Genre</h2>
+        <select value={selectedGenre} onChange={(e) => onFilter(e.target.value)}>
+            <option value="">All Genres</option>
+            {genres.map((genre) => (
+                <option key={genre.id} value={genre.id}>{genre.name}</option>
+            ))}
+        </select>
+    </div>
+); 
 }
 
-
-
-export default GenreFilter
+export default GenreFilter;
