87d1d253e6199445a01e76d3c164b083fcd8324a
Picaso <abcdbohemia@gmail.com>
2025-05-14 19:18:58 +0400

    Feat: adjustments in  GenreFilter.css

--- movie-explorer/src/components/GenreFilter.js
+++ movie-explorer/src/components/GenreFilter.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './GenreFilter.css';
-import SearchBar from './SearchBar';
 
 function GenreFilter({ genres, onFilter, selectedGenre }){
 return (
     <div className="genre-filter">
         <h2>Filter by Genre</h2>
-        <select value={selectedGenre} onChange={(e) => onFilter(e.target.value)}>
+        <select className="genre-select" value={selectedGenre} onChange={(e) => onFilter(e.target.value)}>
             <option value="">All Genres</option>
             {genres.map((genre) => (
                 <option key={genre.id} value={genre.id}>{genre.name}</option>
